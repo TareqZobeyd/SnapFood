f12c41f8025ef9b80fda6cdfa029c0ffe84f104b
Tareq.z <tzobetd65@gmail.com>
2023-11-09 22:28:47 +0330

    feat: complete section comment

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -12,46 +12,62 @@ class CommentController extends Controller
     public function index(Request $request)
     {
         $request->validate([
-            'food_id' => ['nullable', ($request->restaurant_id)],
-            'restaurant_id' => ['nullable', ($request->food_id)],
+            'food_id' => 'nullable|exists:foods,id',
+            'restaurant_id' => 'nullable|exists:restaurants,id',
         ]);
 
+        $user = auth()->user();
+
         if (!is_null($request->food_id)) {
-            $comments = Comment::query()->where(['food_id' => $request->food_id,
-                'user_id' => auth()->user()->id])->orderByDesc('created_at')->get();
-            return response(['Comments' => ($comments)]);
+            $comments = Comment::query()
+                ->where(['food_id' => $request->food_id, 'user_id' => $user->id])
+                ->with(['user', 'order'])
+                ->orderByDesc('created_at')
+                ->get();
+
+            $transformedComments = $comments->map(function ($comment) {
+                return $comment->additional_info;
+            });
+
+            return response(['comments' => $transformedComments]);
         }
 
         if (!is_null($request->restaurant_id)) {
-            $comments = [];
-            $orders = Order::query()->where(['restaurant_id' => $request->restaurant_id,
-                'user_id' => auth()->user()->id])->get();
+            $orders = Order::query()
+                ->where(['restaurant_id' => $request->restaurant_id, 'user_id' => $user->id])
+                ->get();
+
+            $comments = collect([]);
             foreach ($orders as $order) {
-                foreach ($order->comments as $comment) {
-                    $comments[$comment->created_at->format('Y-m-d-H-i-s')] = $comment;
-                }
+                $orderComments = $order->comments->map(function ($comment) {
+                    return $comment->additional_info;
+                });
+                $comments = $comments->concat($orderComments);
             }
-            ksort($comments);
-            return response(['Comments' => ($comments)]);
+
+            $sortedComments = $comments->sortBy('created_at')->values();
+
+            return response(['comments' => $sortedComments]);
         }
     }
+
 
     public function store(Request $request)
     {
         $request->validate([
-            'cart_id' => 'required',
+            'order_id' => 'required',
             'score' => 'required|integer|min:1|max:5',
             'message' => 'required|string',
         ]);
+        $order = $request->order_id;
 
         Comment::query()->create([
-            'order_id' => $request->order_id,
             'user_id' => auth()->user()->id,
+            'order_id' => $order,
             'message' => $request->message,
             'score' => $request->score,
         ]);
 
         return response(['Message' => 'Comment created successfully']);
     }
-
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,7 +8,6 @@ class Comment extends Model
 {
     protected $fillable = [
         'user_id',
-        'food_id',
         'order_id',
         'message',
         'score'
@@ -21,6 +20,23 @@ class Comment extends Model
 
     public function orders()
     {
-        return $this->belongsTo(Order::class);
+        return $this->belongsTo(Order::class, 'order_id');
     }
+
+    public function getAdditionalInfoAttribute()
+    {
+        $additionalInfo = [
+            'author' => [
+                'name' => $this->user->name,
+            ],
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'score' => $this->score,
+            'content' => $this->message,
+        ];
+        if ($this->order) {
+            $additionalInfo['foods'] = $this->order->foods->pluck('name')->all();
+        }
+        return $additionalInfo;
+    }
+
 }
