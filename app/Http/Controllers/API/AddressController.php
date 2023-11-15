7bbf2650adcedc9302a8c68b9ac0ff2433a37577
Tareq.z <tzobetd65@gmail.com>
2023-11-15 13:11:33 +0330

    feat: add setActiveAddress method in address controller for users

--- app/Http/Controllers/API/AddressController.php
+++ app/Http/Controllers/API/AddressController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Address;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class AddressController extends Controller
 {
@@ -39,9 +40,9 @@ class AddressController extends Controller
     {
         $fields = $request->validate([
             'title' => 'required|string',
-            'address' => 'required|string',
-            'latitude' => 'required|numeric',
-            'longitude' => 'required|numeric',
+            'address' => 'required|string|unique:addresses',
+            'latitude' => 'required|numeric|unique:addresses',
+            'longitude' => 'required|numeric|unique:addresses',
         ]);
 
         $address = User::query()->find(auth()->user()->id)->addresses()->create([
@@ -72,5 +73,22 @@ class AddressController extends Controller
         ]);
         $address->update($request->all());
         return response(['Message' => 'Your address is updated successfully']);
+    }
+
+    public function setActiveAddress($id)
+    {
+        $specificAddress = Address::query()->findOrFail($id);
+        $user = auth()->user();
+
+        if ($user->id === $specificAddress->addressable_id && $specificAddress->addressable_type === 'App\\Models\\User') {
+            $user->addresses()->update(['active' => '0']);
+            $specificAddress->update(['active' => '1']);
+            return response([
+                'Message' => 'Your main address is updated'
+            ]);
+        }
+        return response([
+            'Message' => "You don't have access to update this address"
+        ], 403);
     }
 }
