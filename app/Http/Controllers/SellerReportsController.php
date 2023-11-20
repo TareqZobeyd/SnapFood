d3aeeb4e7315c34c28478866fd181b037ff59cb1
Tareq.z <tzobetd65@gmail.com>
2023-11-20 15:52:11 +0330

    feat: change my reports controller methods

--- app/Http/Controllers/SellerReportsController.php
+++ app/Http/Controllers/SellerReportsController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Food;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class SellerReportsController extends Controller
 {
     public function index()
     {
-        $orders = auth()->user()->restaurant->orders;
+        $restaurant = auth()->user()->restaurant;
+        $orders = $restaurant->orders;
         $totalRevenue = $orders->sum('total_amount');
         $foods = Food::all();
 
@@ -18,25 +20,27 @@ class SellerReportsController extends Controller
 
     public function filter(Request $request)
     {
-        $status = $request->input('status');
+        $status = $request->input('seller_status');
         $foodId = $request->input('food_id');
-
+        $foods = Food::all();
+        $orders =Order::all();
         $restaurant = auth()->user()->restaurant;
 
         $query = $restaurant->orders();
 
         if ($status) {
-            $query->where('status', $status);
+            $query->where('seller_status', $status);
         }
 
         if ($foodId) {
-            $query->whereHas('food', function ($foodQuery) use ($foodId) {
+            $query->whereHas('foods', function ($foodQuery) use ($foodId) {
                 $foodQuery->where('id', $foodId);
             });
         }
-        $filteredOrders = $query->get();
-        $totalRevenue = $filteredOrders->sum('total_price');
 
-        return view('seller.reports.index', compact('filteredOrders', 'totalRevenue'));
+        $filteredOrders = $query->get();
+        $totalRevenue = $filteredOrders->sum('total_amount');
+
+        return view('seller.reports.index', compact('filteredOrders', 'totalRevenue', 'foods', 'orders'));
     }
 }
