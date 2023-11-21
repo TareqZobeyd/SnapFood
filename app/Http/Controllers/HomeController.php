4fe3e3278f99dd8f8c7c220a6ccb12557bb9dcd9
Tareq.z <tzobetd65@gmail.com>
2023-11-21 15:01:01 +0330

    feat: display cards of foods in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $foods = Food::all();
+        return view('home', compact('foods'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,4 +9,38 @@
             <p>You have to log in or register first</p>
         </div>
     </div>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <br>
+        <div class="row">
+            @foreach($foods as $food)
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="{{ $food->image_url }}" class="card-img-top" alt="Food Image">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $food->name }}</h5>
+                            <p class="card-text">Price: ${{ $food->price }}</p>
+                            <p class="card-text">Category: {{ $food->categories->name }}</p>
+
+                            @if ($food->custom_discount)
+                                @php
+                                    $discountedPrice = \App\Models\Food::calculateDiscountedPrice($food->price, null, $food->custom_discount);
+                                @endphp
+                                <p class="card-text">Price With Custom Discount: {{ $food->custom_discount }}%</p>
+                                <i class="fas fa-arrow-right" style="color: blue;"></i>
+                                <span style="color: green;">${{ $discountedPrice }}</span>
+                            @elseif ($food->food_discount_id)
+                                @php
+                                    $discountedPrice = \App\Models\Food::calculateDiscountedPrice($food->price, $food->food_discount_id, null);
+                                @endphp
+                                <p class="card-text">Price With Food Discount: {{ $food->food_discount->discount_percentage }}%</p>
+                                <i class="fas fa-arrow-right" style="color: green;"></i>
+                                <span style="color: green;">${{ $discountedPrice }}</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </main>
+
 @endsection
