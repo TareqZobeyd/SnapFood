5b0e376bd72825f9a71addbe0093d0bdd71384a7
Tareq.z <tzobeyd65@gmail.com>
2024-04-15 16:38:52 +0330

    feat: add restaurant resources

---
+++ app/Http/Resources/RestaurantResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RestaurantResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->name,
+            'address' => [
+                'address' => $this->address,
+                'latitude' => $this->latitude,
+                'longitude' => $this->longitude,
+            ],
+            'is_open' => (bool) $this->is_open,
+            'score' => $this->score ?? null,
+        ];
+    }
+}
